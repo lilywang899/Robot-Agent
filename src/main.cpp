694ed0ee9fe79ea213528df0bfe79b3f69acfadf
lily.jyw <lily@ubuntu-jammy.myguest.virtualbox.org>
2025-05-13 22:17:56 -0400

    working version on windows vm

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, const char **argv)
     spdlog::warn("Easy padding in numbers like {:08d}", 12);
     spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR,SPDLOG_VER_PATCH);
     std::string mode;
-    std::string configFile = "../../config/config.txt";
+    std::string configFile = "/home/lily/Robot-Agent/config/config.txt";
     loadConfig(configFile,mode);
     if (mode == "robot") 
     {
